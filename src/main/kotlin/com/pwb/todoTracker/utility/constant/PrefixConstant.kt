8f148a9680101fa400c3726a3e51b90faf6295d2
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-19 23:56:40 +0100

    Initial commit for TodoTracker

---
+++ src/main/kotlin/com/pwb/todoTracker/utility/constant/PrefixConstant.kt
@@ -0,0 +1,6 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-19T22:55:32.912412900
+package com.pwb.todoTracker.utility.constant
+
+object PrefixConstant {
+    const val ENTITY = "DBO."
+}
