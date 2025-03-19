ae8735ede113f23e245fa600fcb933c2035e4d45
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-19 23:55:45 +0100

    Initial commit for TodoTracker

---
+++ src/main/kotlin/com/pwb/todoTracker/config/SwaggerConfig.kt
@@ -0,0 +1,35 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-19T22:55:32.912412900
+package com.pwb.todoTracker.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.pwb.todoTracker"))
+            .paths(PathSelectors.any())
+            .build()
+            .pathMapping("api")
+            .apiInfo(apiInfo())
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+            .title("TodoTracker Documentation")
+            .description("TodoTracker Documentation")
+            .version("1.0.0")
+            .build()
+    }
+}
