54f5bc19f1528a59e21ad966f07023f2d0a006eb
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-19 23:56:41 +0100

    Initial commit for TodoTracker

---
+++ src/main/kotlin/com/pwb/todoTracker/utility/constant/PrivilegeConstant.kt
@@ -0,0 +1,11 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-19T22:55:32.912412900
+package com.pwb.todoTracker.utility.constant
+
+object PrivilegeConstant {
+    const val CREATE = "CREATE"
+    const val UPDATE = "UPDATE"
+    const val READ = "READ"
+    const val DELETE = "DELETE"
+    const val CHECKER = "CHECKER"
+    const val ALL = "*"
+}
