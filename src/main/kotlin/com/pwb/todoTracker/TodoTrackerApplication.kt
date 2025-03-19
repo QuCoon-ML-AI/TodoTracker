8f8c353379c0a29ca189cad39c964f720a7075cd
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-19 23:56:39 +0100

    Initial commit for TodoTracker

---
+++ src/main/kotlin/com/pwb/todoTracker/TodoTrackerApplication.kt
@@ -0,0 +1,13 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-19T22:55:32.912412900
+package com.pwb.todoTracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication< TodoTrackerApplication>(*args)
+}
+
